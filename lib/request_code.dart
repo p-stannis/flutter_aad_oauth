58c010779fc134bf0657473736b8570792265591
Leonardo Leite <42722914+LeonardoLeiteMeira@users.noreply.github.com>
2021-05-12 15:30:47 -0300

    fixed url error

--- lib/request_code.dart
+++ lib/request_code.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart'
     show MaterialPageRoute, Navigator, SafeArea, Scaffold;
+import 'package:flutter/widgets.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'model/config.dart';
 import 'request/authorization_request.dart';
@@ -21,8 +22,10 @@ class RequestCode {
     final String urlParams = _constructUrlParams();
 
     if (_config.context != null) {
+      String initialURL =
+          ("${_authorizationRequest.url}?$urlParams").replaceAll(" ", "%20");
       var web = WebView(
-        initialUrl: "${_authorizationRequest.url}?$urlParams",
+        initialUrl: initialURL,
         javascriptMode: JavascriptMode.unrestricted,
         onPageFinished: (url) {
           Uri uri = Uri.parse(url);
@@ -40,7 +43,9 @@ class RequestCode {
       );
 
       await Navigator.of(_config.context!).push(MaterialPageRoute(
-          builder: (context) => Scaffold(body: SafeArea(child: web))));
+          builder: (context) => Scaffold(
+                body: SafeArea(child: web),
+              )));
     } else {
       throw Exception("Context is null. Please call setContext(context).");
     }
